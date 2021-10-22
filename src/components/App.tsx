304edb8544d9b523e86f1005b3cb58ca6ebeadb6
Felix-Karl <karl@Karls-MBP.fritz.box>
2021-10-22 15:16:45 +0200

    homepage component, folder structure

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import { HomePage } from "./Homepage/HomepageComponent";
+
+function App(): JSX.Element {
+  return <HomePage />;
+}
+
+export default App;
