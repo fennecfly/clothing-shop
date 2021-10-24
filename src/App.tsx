32e681e46f14fc49ac74b19ac68e8fff11925345
bloody-fly <felix.karl442@gmail.com>
2021-10-24 21:28:56 +0200

    deploying with gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { Route, Switch } from "react-router";
+import { HashRouter } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/Homepage/HomepageComponent";
 import ShopPage from "./pages/ShopPage/ShopPageComponent";
 
 function App(): JSX.Element {
   return (
-    <div>
+    <HashRouter basename="/">
       <Switch>
         <Route exact={true} path="/" component={HomePage} />
 
         <Route path="/shop" component={ShopPage} />
       </Switch>
-    </div>
+    </HashRouter>
   );
 }
 
