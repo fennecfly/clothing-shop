16c3486473ca67cf61017bd98726ba4191e7bb35
bloody-fly <felix.karl442@gmail.com>
2021-10-28 19:53:08 +0200

    box-sizing and google sign in fix

--- src/firebase/firebase.utils.ts
+++ src/firebase/firebase.utils.ts
@@ -4,6 +4,8 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import "firebase/compat/firestore";
+import { SyntheticEvent } from "react";
+import { rethrow } from "../helpers/rethrow";
 
 const config = {
   apiKey: "AIzaSyACkuYxJxJ4EUu3fCkAITexymGNRXPuOJ0",
@@ -22,7 +24,9 @@ export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
-export const signInWithGoogle = (): Promise<firebase.auth.UserCredential> =>
-  auth.signInWithPopup(provider);
+export const signInWithGoogle = (event: SyntheticEvent): void => {
+  event.preventDefault();
+  auth.signInWithPopup(provider).catch(rethrow);
+};
 
 export default firebase;
